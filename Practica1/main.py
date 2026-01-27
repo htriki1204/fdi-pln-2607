e9df0d8ced1912dc7635c60fc6545f1e6efc6262
htriki1204 <htriki@ucm.es>
2026-01-27 17:26:40 +0100

    implementa funciones para los principales endpoints

--- Practica1/main.py
+++ Practica1/main.py
@@ -1,16 +1,52 @@
+import requests
+import json
 
-def main():    
-    import requests
-    url = "http://147.96.81.252:8000"
+BASE_URL = "http://147.96.81.252:8000"
 
-    nombre = "alvaroYhamza"
-    response = requests.post(f"{url}/alias/{nombre}")
+def get_info():
+    url = f"{BASE_URL}/info"
 
-    if response.status_code == 200:
-        print(response.json())
-    else:
-        print("Error:", response.status_code, response.text)
+    response = requests.get(url)
+    return response.json()
+
+
+def post_alias(nombre: str):
+    """Crear alias"""
+    url = f"{BASE_URL}/alias/{nombre}"
+    response = requests.post(url)
+    return response.json()
+
+
+def delete_alias(nombre: str):
+    """Eliminar alias"""
+    url = f"{BASE_URL}/alias/{nombre}"
+    response = requests.delete(url)
+    return response.json()
+
+
+def post_carta(remi: str, dest: str, asunto: str, cuerpo: str, id_: str):
+    """Crear carta"""
+    url = f"{BASE_URL}/carta"
+    payload = {
+        "remi": remi,
+        "dest": dest,
+        "asunto": asunto,
+        "cuerpo": cuerpo,
+        "id": id_
+    }
+    response = requests.post(url, json=payload)
+    return response.json()
 
 
 if __name__ == "__main__":
-    main()
+
+    alias = "trumpeta"
+
+    alias_result = post_alias(alias)
+    print(f"POST /alias/{alias} ->", alias_result)
+    
+    info_result = get_info()
+    print(f"GET /get_info/ ->", info_result)
+    print(json.dumps(info_result, indent=4))
+
+
